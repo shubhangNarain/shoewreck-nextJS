114635bbb037ae3dd22fcc62e9ea975730171eb8
shubhangNarain <snvishere@gmail.com>
2023-04-17 10:56:50 +0530

    added a few assets for the website, made a few changes to the components

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, {useState, useEffect}from "react";
+
+import { Wrapper } from "./Wrapper";
+import Link from "next/link";
 
 const Header = () => {
-    return <div className="text">Header</div>;
+    const [mobileMent, setMobileMent] = useState(false);
+    const [showCatMenu, setShowCatMenu] = useState(false);
+    const [show, setShow] = useState("translate-y-0");
+    const [lastScrollY, setLastScrollY] = useState({});
+
+    return (
+        <>
+            <header className={`w-full h-[50px] md-[80px] bg-white flex items-center justify-between z-20 sticky top-0 transition-transform duration-300 ${show}`}>
+                <Wrapper>
+                    <Link href="/">
+                        <img src="/logo.svg" className="w-[40px] md:w-[60px]" />
+                    </Link>
+                </Wrapper>
+            </header>
+        </>
+    )
 };
 
 export default Header;
---
+++ components/Menu.jsx
@@ -0,0 +1,32 @@
+import Link from 'next/link';
+import React from 'react'
+import { BsChevronDown } from 'react-icons/bs';
+
+const data = [
+    { id: 1, name: "Home", url: "/" },
+    { id: 2, name: "About", url: "/about" },
+    { id: 3, name: "Categories", subMenu: true },
+    { id: 4, name: "Contact", url: "/contact" },
+];
+
+const subMenuData = [
+    { id: 1, name: "Jordan", doc_count: 11 },
+    { id: 2, name: "Sneakers", doc_count: 8 },
+    { id: 3, name: "Running shoes", doc_count: 64 },
+    { id: 4, name: "Football shoes", doc_count: 107 },
+];
+
+const Menu = () => {
+  return (
+    <ul className='hidden md:flex items-center gap-8 font-medium text-black'>
+        {data.map((item) => {
+            return (
+                <React.Fragment key={item.id}></React.Fragment>
+            )
+        })}
+            
+    </ul>
+  )
+}
+
+export default Menu
