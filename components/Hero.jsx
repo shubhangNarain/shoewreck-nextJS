e22edef1058eaebe62b4b2fc3a099d79fe8bed72
shubhangNarain <snvishere@gmail.com>
2023-04-18 18:47:28 +0530

    Finished navbar styling and the homepage layout

---
+++ components/Hero.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+
+import { BiArrowBack, BiArrowForward } from "react-icons/bi";
+
+const Hero = () => {
+  return (
+    <>
+        <div className="relative text-white text-[20px] w-ufll max-w-[1360px] mx-auto">
+      <Carousel
+        autoPlay={true}
+        infiniteLoop={true}
+        interval={1000}
+        showThumbs={false}
+        showIndicators={false}
+        showStatus={false}
+        renderArrowPrev={(clickHandler, hasPrev) => {
+            return (
+                <div
+                onClick={clickHandler}
+                className="absolute right-[31px] md:right-[51px] bottom-0 w-[30px]
+                md:w-[50px] h-[30px] md:h-[50px] bg-black z-10 flex items-center
+                justify-center cursor-pointer hover:opacity-80"
+                >
+                <BiArrowBack className="text-[25px]" />
+                </div>
+            );
+        }}
+        renderArrowNext={(clickHandler, hasNext) => {
+            return (
+                <div
+                onClick={clickHandler}
+                className="absolute right-0 bottom-0 w-[30px] md:w-[50px] h-[30px] 
+                md:h-[50px] bg-black z-10 flex items-center justify-center
+                cursor-pointer hover:opacity-80"
+                >
+                <BiArrowBack className="rotate-180 text-sm md:text-lg" />
+                </div>
+            );
+        }}
+      >
+        <div>
+          <img 
+            src="/slide-1.png"
+            className="aspect-[16/10] md:aspect-auto object-cover" 
+            />
+          <div className="px-[15px] md:px-[40px] py-[10px] md:py-[25px]
+          font-oswald bg-white absolute bottom-[25px] md:bottom-[75px] left-0
+          text-black/[0.9] text-[15px] md:text-[30px] uppercase font-medium
+          cursor-pointer hover:opacity-90">
+            Shop Now
+          </div>
+        </div>
+
+        <div>
+          <img 
+            src="/slide-2.png"
+            className="aspect-[16/10] md:aspect-auto object-cover" 
+            />
+          <div className="px-[15px] md:px-[40px] py-[10px] md:py-[25px]
+          font-oswald bg-white absolute bottom-[25px] md:bottom-[75px] left-0
+          text-black/[0.9] text-[15px] md:text-[30px] uppercase font-medium
+          cursor-pointer hover:opacity-90">
+            Shop Now
+          </div>
+        </div>
+
+        <div>
+          <img 
+            src="/slide-3.png"
+            className="aspect-[16/10] md:aspect-auto object-cover" 
+            />
+          <div className="px-[15px] md:px-[40px] py-[10px] md:py-[25px]
+          font-oswald bg-white absolute bottom-[25px] md:bottom-[75px] left-0
+          text-black/[0.9] text-[15px] md:text-[30px] uppercase font-medium
+          cursor-pointer hover:opacity-90">
+            Shop Now
+          </div>
+        </div>
+      </Carousel>
+    </div>
+    </>
+  );
+};
+
+export default Hero;
