5855a15fa61601d91e21f27b153b9ca6491399cc
shubhangNarain <snvishere@gmail.com>
2023-05-01 23:59:43 +0530

    added next image config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,  
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    images: {
+      domains: ['res.cloudinary.com'],
+    }
 }
 
 module.exports = nextConfig
