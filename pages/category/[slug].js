dba96f5c9bc484b7c275b2bb2509fcb30c2199ed
shubhangNarain <snvishere@gmail.com>
2023-05-03 08:11:13 +0530

    removed static product cards

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -19,15 +19,6 @@ const Category = (category, products, slug) => {
           className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5
             my-14 px-5 md:px-0"
         >
-          {/* <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard /> */}
         </div>
         {/* product cards end */}
       </Wrapper>
